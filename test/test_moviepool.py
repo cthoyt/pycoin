e4b592ac6adf7439f9af29b7cf30387922f9d385
Scott Colby <scolby33@gmail.com>
2015-05-14 13:56:03 -0400

    Update test to use monkey patched version of tmdbsimple
    Prevent need for network connection for testing MoviePool functionality

--- test/test_moviepool.py
+++ test/test_moviepool.py
@@ -8,18 +8,32 @@ from pycoin import moviepool as mp
 def testPool():
     return mp.MoviePool()
     
+@pytest.fixture
+def fake_tmdbsimple(monkeypatch):
+    monkeypatch.setattr('tmdbsimple.Movies', FakeTmdbsimpleMovies)
+
+class FakeTmdbsimpleMovies():
+    def __init__(self, identifier):
+        self.title = 'Example Movie'
+        self.vote_average = 10.0
+    
+    def info(self):
+        pass
+    
+    
+    
 def test_creation_of_MoviePool(testPool):
     assert isinstance(testPool, mp.MoviePool)
     
 def test_new_MoviePool_has_empty_pool(testPool):
     assert testPool.pool == {}
     
-def test_add_movie_by_tmdb_id_to_MoviePool(testPool):
-    testPool.add_movie(603) # tmdb id for 'The Matrix'
+def test_add_movie_by_tmdb_id_to_MoviePool(testPool, fake_tmdbsimple):
+    testPool.add_movie(1)
     assert testPool.pool == {
-        603: {
-            'title': 'The Matrix',
-            'vote_average': 7.6,
+        1: {
+            'title': 'Example Movie',
+            'vote_average': 10.0,
             'watched': False
         }
     }
