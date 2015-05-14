3693f416a9131c90556a9b19f4958f0a4d9a5a28
Scott Colby <scolby33@gmail.com>
2015-05-14 14:24:51 -0400

    Finish implementation of MoviePool.add_movie()
    Can now add by tmdb id, imdb id, or title. Movies cannot be added twice.

--- pycoin/moviepool.py
+++ pycoin/moviepool.py
@@ -2,16 +2,36 @@
 
 import tmdbsimple as tmdb
 
+tmdb.API_KEY = 'f184fcf64fe19365d01d5bec547156ef'
+
+class MoviePoolDuplicateMovieError(Exception):
+    pass
+
 class MoviePool():
     def __init__(self):
         self.pool = {}
         
+    def _tmdb_id_from_identifier(self, identifier):
+        if identifier[:2] == 'tt':  # we have an IMDb id
+            tmdb_id = tmdb.Find(identifier).info(external_source = 'imdb_id')['movie_results'][0]['id']
+        elif not identifier.isdigit(): # we must have a title, since tmdb id's are ints
+            tmdb_id = tmdb.Search().movie(query = identifier)['results'][0]['id']
+        else:
+            tmdb_id = int(identifier)
+        
+        return tmdb_id
+        
     def add_movie(self, identifier):
-        movie = tmdb.Movies(identifier)
+        tmdb_id = self._tmdb_id_from_identifier(identifier)
+        
+        movie = tmdb.Movies(tmdb_id)
         response = movie.info()
         
-        self.pool[identifier] = {
-            'title': movie.title,
-            'vote_average': movie.vote_average,
-            'watched': False
-        }
+        if tmdb_id not in self.pool:
+            self.pool[tmdb_id] = {
+                'title': movie.title,
+                'vote_average': movie.vote_average,
+                'watched': False
+            }
+        else:
+            raise MoviePoolDuplicateMovieError
--- test/test_moviepool.py
+++ test/test_moviepool.py
@@ -3,6 +3,7 @@
 import pytest
 
 from pycoin import moviepool as mp
+from pycoin.moviepool import MoviePoolDuplicateMovieError
 
 @pytest.fixture
 def testPool():
@@ -11,16 +12,47 @@ def testPool():
 @pytest.fixture
 def fake_tmdbsimple(monkeypatch):
     monkeypatch.setattr('tmdbsimple.Movies', FakeTmdbsimpleMovies)
+    monkeypatch.setattr('tmdbsimple.Find', FakeTmdbsimpleFind)
+    monkeypatch.setattr('tmdbsimple.Search', FakeTmdbsimpleSearch)
 
 class FakeTmdbsimpleMovies():
-    def __init__(self, identifier):
-        self.title = 'Example Movie'
-        self.vote_average = 10.0
+    _example_movie_data = {
+        1: {
+            'title': 'Example Movie',
+            'vote_average': 10.0
+        }
+    }
+    
+    def __init__(self, tmdb_id):
+        self.tmdb_id = tmdb_id
     
     def info(self):
+        self.title = self._example_movie_data[self.tmdb_id]['title']
+        self.vote_average = self._example_movie_data[self.tmdb_id]['vote_average']
+    
+class FakeTmdbsimpleFind():
+    def __init__(self, identifier):
         pass
-    
-    
+        
+    def info(self, external_source):
+        return {
+            'movie_results': [
+                {'id': 1}
+            ]
+        }
+        
+class FakeTmdbsimpleSearch():
+    def __init__(self):
+        pass
+        
+    def movie(self, query):
+        return {
+            'results': [
+                {
+                    'id': 1
+                }
+            ]
+        }
     
 def test_creation_of_MoviePool(testPool):
     assert isinstance(testPool, mp.MoviePool)
@@ -29,7 +61,7 @@ def test_new_MoviePool_has_empty_pool(testPool):
     assert testPool.pool == {}
     
 def test_add_movie_by_tmdb_id_to_MoviePool(testPool, fake_tmdbsimple):
-    testPool.add_movie(1)
+    testPool.add_movie('1')
     assert testPool.pool == {
         1: {
             'title': 'Example Movie',
@@ -37,4 +69,29 @@ def test_add_movie_by_tmdb_id_to_MoviePool(testPool, fake_tmdbsimple):
             'watched': False
         }
     }
+    
+def test_add_movie_by_imdb_id_to_MoviePool(testPool, fake_tmdbsimple):
+    testPool.add_movie('tt1')
+    assert testPool.pool == {
+        1: {
+            'title': 'Example Movie',
+            'vote_average': 10.0,
+            'watched': False
+        }
+    }
+    
+def test_add_movie_by_title_to_MoviePool(testPool, fake_tmdbsimple):
+    testPool.add_movie('Example Movie')
+    assert testPool.pool == {
+        1: {
+            'title': 'Example Movie',
+            'vote_average': 10.0,
+            'watched': False
+        }
+    }
+    
+def test_cannot_add_movie_twice_to_MoviePool(testPool, fake_tmdbsimple):
+    testPool.add_movie('1')
+    with pytest.raises(MoviePoolDuplicateMovieError):
+        testPool.add_movie('1')
     
