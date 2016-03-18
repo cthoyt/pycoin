24b1c57d292336128c1060be21dcef0602041daa
Scott Colby <scolby33@gmail.com>
2016-03-18 03:33:21 -0700

    Complete re-write
    TDD ground this project to a halt. Here it is, almost complete and
    working.

--- setup.py
+++ setup.py
@@ -1,39 +1,79 @@
-#!/usr/bin/env python3
+"""Setup module for the pycoin script"""
 
-import sys
+import setuptools
+import codecs  # To use a consistent encoding
+import os
+import re
 
-from setuptools import setup
-from setuptools.command.test import test as TestCommand
+#################################################################
 
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-    
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-        
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-        
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
+PACKAGES = setuptools.find_packages(where='src')
+META_PATH = os.path.join('src', 'pycoin', '__init__.py')
+KEYWORDS = ['movies', 'coin', 'what to watch']
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: End Users/Desktop',
+    'License :: Other/Proprietary License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Multimedia :: Video',
+    'Topic :: Utilities'
+]
+INSTALL_REQUIRES = ['click', 'py', 'requests', 'tmdbsimple']
+EXTRAS_REQUIRE = {}
 
-setup(
-    name = 'pycoin',
-    packages = ['pycoin'],
-    install_requires = [
-        'tmdbsimple==1.3.0',
-        'requests==2.5.3'
-    ],
-    tests_require = [
-        'pytest',
-        'pytest-cov',
-        'pytest-mock'
-    ],
-    cmdclass = {'test': PyTest}
-)
+#################################################################
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """Build an absolute path from *parts* and return the contents of the resulting file. Assume UTF-8 encoding."""
+    with codecs.open(os.path.join(HERE, *parts), 'rb', 'utf-8') as f:
+        return f.read()
+
+META_FILE = read(META_PATH)
+
+
+def find_meta(meta):
+    """Extract __*meta*__ from META_FILE"""
+    meta_match = re.search(
+        r'^__{meta}__ = ["\']([^"\']*)["\']'.format(meta=meta),
+        META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError('Unable to find __{meta}__ string'.format(meta=meta))
+
+
+def get_long_description():
+    """Get the long_description from the README.md file. Assume UTF-8 encoding."""
+    with codecs.open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name=find_meta('title'),
+        version=find_meta('version'),
+        description=find_meta('description'),
+        long_description=get_long_description(),
+        url=find_meta('url'),
+        author=find_meta('author'),
+        author_email=find_meta('email'),
+        maintainer=find_meta('author'),
+        license=find_meta('license'),
+        classifiers=CLASSIFIERS,
+        keywords=KEYWORDS,
+        packages=PACKAGES,
+        package_dir={'': 'src'},
+        install_requires=INSTALL_REQUIRES,
+        extras_require=EXTRAS_REQUIRE,
+        entry_points={
+            'console_scripts': [
+                'pycoin=pycoin.cli:cli'
+            ]
+        }
+    )
