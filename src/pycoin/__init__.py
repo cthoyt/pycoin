0a9474db708d3a221a782a91bc0f4b0b0cb42e0e
Scott Colby <scolby33@gmail.com>
2016-03-18 03:36:23 -0700

    Fix some renaming issues

--- src/pycoin/__init__.py
+++ src/pycoin/__init__.py
@@ -1,8 +1,8 @@
 __version__ = '0.0.1'
 
 __title__ = 'Pycoin'
-__description__ = ''
-__url__ = ''
+__description__ = 'pycoin is a command line utility to semi-randomly choose a movie to watch from the list of movies you would like to see.'
+__url__ = 'https://github.com/scolby33/pycoin'
 
 __author__ = 'Scott Colby'
 __email__ = 'scolby33@gmail.com'
